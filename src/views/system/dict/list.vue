24669e9cc5e04cf687f8e72979c254ffe56412e5
joygqz <73623026+joygqz@users.noreply.github.com>
2025-07-29 08:57:46 +0800

    feat: 冲突解决

--- src/views/system/dict/list.vue
+++ src/views/system/dict/list.vue
@@ -3,26 +3,14 @@ defineOptions({
   name: 'DictList',
 })
 const router = useRouter()
-<<<<<<< HEAD
-=======
-function onCreate() {
-  router.push({ name: 'systemDictCreate' })
-}
->>>>>>> 2b660a7 (feat: 激活导航开发)
 </script>
 
 <template>
   <VPage>
     字典管理
-<<<<<<< HEAD
     <input type="text">
     <button @click="router.push({ name: 'SystemDictCreate' })">
       创建
     </button>
-=======
-    <ElButton @click="onCreate">
-      创建字典
-    </ElButton>
->>>>>>> 2b660a7 (feat: 激活导航开发)
   </VPage>
 </template>
