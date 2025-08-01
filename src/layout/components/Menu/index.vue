d114bb85e3a53f95469c29966eb665c601245b9c
joygqz <73623026+joygqz@users.noreply.github.com>
2025-08-01 09:38:36 +0800

    fix: 菜单激活样式

--- src/layout/components/Menu/index.vue
+++ src/layout/components/Menu/index.vue
@@ -19,7 +19,7 @@ const menuStore = useMenuStore()
   border-right: 0;
   --el-menu-item-height: 48px;
   --el-menu-sub-item-height: var(--el-menu-item-height);
-  --el-menu-active-color: var(--el-text-color-primary);
+  --el-menu-active-color: hsl(var(--primary-foreground));
   --el-menu-bg-color: transparent;
 
   :deep(.el-menu-item.is-active) {
